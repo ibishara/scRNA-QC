dd455e681cd8c55f1855cba16c8797bf4dc38494
Isaac Bishara <ibishara@coh34484.lan>
2022-04-28 22:17:44 -0700

    Added LQ processing

--- 001_subsampling_C1.r
+++ 001_subsampling_C1.r
@@ -1,6 +1,11 @@
-## This script randomly subsamples 20% cells from each batch pre- and post-filtering from metadata and raw counts (FELINE cohort 1)
+## This script consists of 3 sections :
+## 1. Subsample n cells from High Quality cells metadata including cell type annotaions (cite). Cell type annotations are used to construct lineage annotations.
+## 2. Subsample read count matrix for corresponding high quality cells. HQ counts and metadata are used to construct HQ_seurat obj.
+## 3. Subsample n cells from non-HQ cells (low quality cells) metadata.
+## Note: nCounts and nFeature columns are recalculated from corresponding count matrix 
 
-
+## HQ cells use JF's annotations 'Celltype', 'Annotation'
+## LQ cells use hpca SingleR annotations
 # Author: "Isaac Bishara"
 
 setwd('/Users/ibishara/Desktop/FELINE_C1/')
@@ -9,7 +14,12 @@ setwd('/Users/ibishara/Desktop/FELINE_C1/')
 library(data.table)
 library(stringr)
 library(tidyverse)
+library(parallel)
+library(Seurat)
+library(qs)
 
+numCores <- detectCores()
+numCores 
 
 # data
 ref1 <- as.data.frame(fread('post-filter/Feline_metadata_101421.txt', sep='auto')) # post-filtered metadata as a reference for "high-quality" cells | 17 celltype classes
@@ -23,36 +33,119 @@ common.cells <- intersect(ref1$Cell, ref2$Cell)
 ref2 <- ref2[common.cells, ]
 ref1 <- ref1[common.cells, ]
 
-# filtered_HQ_cellID <- ref1[ref1$Platform == '10x','Cell.ID'] # cell ids filtered for 10X technology 
-meta_HQ <- left_join(ref1, ref2[, c('Cell','Celltype')], by = 'Cell' ) # combine both cell annotations
-
+# FELINE C1 high quality metadata with cell type annotations 
+meta_HQ <- left_join(ref1, ref2[, c('Cell','Celltype')], by = 'Cell' ) # combine both cell type annotations
 
 set.seed(100)
-## metadata subsampling
-meta_HQ_sub <- meta_HQ[sample(nrow(meta_HQ), round(0.2 * nrow(meta_HQ))), ] # subsample 10% cells 
+sample.ncell <- 35000 # number of cells to sample for HQ and LQ
 
-## Subsample count table for matching cell ids 
-## Takes forever to run in the notebook. Run in standard R or Radian
+## HQ metadata subsampling ##
+meta_HQ_sub <- meta_HQ[sample(nrow(meta_HQ), sample.ncell), ] # subsample 20% cells 
+
+# JF's annotations "Celltype" are used for lineage annotation of high quality cells 
+meta_HQ_sub[["Lineage"]] <- meta_HQ_sub$Celltype
+meta_HQ_sub <- meta_HQ_sub %>% mutate( Lineage = case_when(
+# relabel HQ cells according to JF annotations | Only HQ cells have "Celltype" annotaions
+        meta_HQ_sub[["Celltype"]] == "Cancer cells"  ~ 'Epithelial_cells',
+        meta_HQ_sub[["Celltype"]] == "Normal epithelial cells"  ~ 'Epithelial_cells',
+        meta_HQ_sub[["Celltype"]] == "Adipocytes"  ~ 'Mesenchymal_cells',
+        meta_HQ_sub[["Celltype"]] == "Fibroblasts"  ~ 'Mesenchymal_cells',
+        meta_HQ_sub[["Celltype"]] == "Endothelial cells"  ~ 'Mesenchymal_cells',
+        meta_HQ_sub[["Celltype"]] == "Pericytes"  ~ 'Mesenchymal_cells',
+        meta_HQ_sub[["Celltype"]] == "Macrophages"  ~ 'Hematopoeitic_cells',
+        meta_HQ_sub[["Celltype"]] == "T cells"  ~ 'Hematopoeitic_cells',
+        meta_HQ_sub[["Celltype"]] == "B cells"  ~ 'Hematopoeitic_cells'
+))
+
+
+
+## Subsample count table for matching cell ids ##
+## Inefficient to run in a notebook. Run in base R or Radian
 ## creates and index of subsamples cells in metadata for each raw count table then subsample each table only for matching cells
+system.time({
 all.counts <- list.files(path = "/Users/ibishara/Desktop/FELINE_C1/raw/FELINE_cellranger_premRNA/", pattern = "*counts.txt", recursive = TRUE) # create a list of raw count tables from all batches
-count.filter.list <- lapply(paste('raw/FELINE_cellranger_premRNA/', all.counts, sep=''), FUN = function(x) {
+count.filter.list <- mclapply(paste('raw/FELINE_cellranger_premRNA/', all.counts, sep=''), FUN = function(x) {
 count_sub <- fread(x, sep='auto', select= c("Gene Symbol",  meta_HQ_sub$Cell  )) # cell ids subsampled from the metadata
 return(count_sub)  # returns warnings for every cell id NOT in each count table
-})
+}, mc.cores= numCores)
 
-count_batches_merged <- as.data.frame(do.call(cbind, count.filter.list))  #join list of tables 
-count_batches_merged <- count_batches_merged[,!duplicated(colnames(count_batches_merged))] # remove duplicate cell id
-count_batches_merged <- count_batches_merged[!duplicated(count_batches_merged$"Gene Symbol"),] # remove duplicate genes
-count_batches_merged <- count_batches_merged[count_batches_merged$'Gene Symbol' != '' ,] # remove null genes
+HQ_count_batches_merged <- as.data.frame(do.call(cbind, count.filter.list))  #join list of tables 
+HQ_count_batches_merged <- HQ_count_batches_merged[,!duplicated(colnames(HQ_count_batches_merged))] # remove duplicate cell id
+HQ_count_batches_merged <- HQ_count_batches_merged[!duplicated(HQ_count_batches_merged$"Gene Symbol"),] # remove duplicate genes
+HQ_count_batches_merged <- HQ_count_batches_merged[HQ_count_batches_merged$'Gene Symbol' != '' ,] # remove null genes
 
 # ordering counts and meta cells alphabetically 
-temp <- count_batches_merged[,-1]
+temp <- HQ_count_batches_merged[,-1]
 sorted_counts <- temp[, order(colnames(temp))] 
 sorted_meta <- meta_HQ_sub[order(meta_HQ_sub$Cell) ,]
 
-meta_HQ_sub$nCount_RNA = colSums(sorted_counts)  # corrected nCount_RNA
+sorted_meta$nCount_RNA = colSums(sorted_counts)  # corrected nCount_RNA
 sorted_counts[sorted_counts > 0] <- 1
-meta_HQ_sub$nFeature_RNA = colSums(sorted_counts)  # corrected nFeature_RNA
+sorted_meta$nFeature_RNA = colSums(sorted_counts)  # corrected nFeature_RNA
+meta_HQ_sub <- sorted_meta
 
+})
+
+fwrite(HQ_count_batches_merged, 'raw_counts_subsample_HQ.txt', sep='\t') # Export subsampled raw counts
 fwrite(meta_HQ_sub, 'metadata_subsample_HQ.txt', sep='\t') # Export subsampled metadata
-fwrite(count_batches_merged, 'raw_counts_subsample_HQ.txt', sep='\t') # Export subsampled raw counts
+
+# construct HQ seurat object
+seu_HQ <- CreateSeuratObject(counts= HQ_count_batches_merged, min.features= 0, min.cells = 0, names.delim= "_", meta.data= meta_HQ_sub) 
+qsave(seu_HQ, file="seu_HQ.qs")
+
+
+
+
+
+## LQ metadata subsampling ##
+meta.data  <- as.data.frame(fread('raw/FELINE_C1_raw_singler_metadata.txt', sep='\t')) # Full FELINE C1 metadata + SingleR annotations
+
+meta_LQ <- meta.data[ !meta.data$Cell %in% meta_HQ$Cell, ] # remove HQ cells 
+meta_LQ_sub <- meta_LQ[sample(nrow(meta_LQ), sample.ncell), ] #
+meta_LQ_sub$V1 <- NULL
+
+# Read corresponding cell counts to correct nCounts_RNA and nFeature_RNA 
+all.counts <- list.files(path = "/Users/ibishara/Desktop/FELINE_C1/raw/FELINE_cellranger_premRNA/", pattern = "*counts.txt", recursive = TRUE) # create a list of raw count tables from all batches
+count.filter.list <- mclapply(paste('raw/FELINE_cellranger_premRNA/', all.counts, sep=''), FUN = function(x) {
+count_sub <- fread(x, sep='auto', select= c("Gene Symbol",  meta_LQ_sub$Cell  )) # cell ids subsampled from the metadata
+return(count_sub)  # returns warnings for every cell id NOT in each count table
+}, mc.cores= numCores)
+
+LQ_count_batches_merged <- as.data.frame(do.call(cbind, count.filter.list))  #join list of tables 
+LQ_count_batches_merged <- LQ_count_batches_merged[,!duplicated(colnames(LQ_count_batches_merged))] # remove duplicate cell id
+LQ_count_batches_merged <- LQ_count_batches_merged[!duplicated(LQ_count_batches_merged$"Gene Symbol"),] # remove duplicate genes
+LQ_count_batches_merged <- LQ_count_batches_merged[LQ_count_batches_merged$'Gene Symbol' != '' ,] # remove null genes
+
+# ordering counts and meta cells alphabetically 
+temp <- LQ_count_batches_merged[,-1]
+sorted_counts <- temp[, order(colnames(temp))] 
+sorted_meta <- meta_LQ_sub[order(meta_LQ_sub$Cell) ,]
+
+sorted_meta$nCount_RNA = colSums(sorted_counts)  # corrected nCount_RNA
+sorted_counts[sorted_counts > 0] <- 1
+sorted_meta$nFeature_RNA = colSums(sorted_counts)  # corrected nFeature_RNA
+meta_LQ_sub <- sorted_meta
+
+
+# Lineage annotations based off hpca annotations 
+meta_LQ_sub[["Lineage"]] <- meta_LQ_sub$hpca
+meta_LQ_sub <- meta_LQ_sub %>% mutate( Lineage = case_when(
+        meta_LQ_sub[["hpca"]] == "Epithelial_cells"  ~ 'Epithelial_cells',
+        meta_LQ_sub[["hpca"]] == "Fibroblasts"  ~ 'Mesenchymal_cells',
+        meta_LQ_sub[["hpca"]] == "Smooth_muscle_cells"  ~ 'Mesenchymal_cells',
+        meta_LQ_sub[["hpca"]] == "Endothelial_cells"  ~ 'Mesenchymal_cells',
+        meta_LQ_sub[["hpca"]] == "Chondrocytes"  ~ 'Mesenchymal_cells',
+        meta_LQ_sub[["hpca"]] == "Osteoblasts"  ~ 'Mesenchymal_cells',
+        meta_LQ_sub[["hpca"]] == "T_cells"  ~ 'Hematopoeitic_cells',
+        meta_LQ_sub[["hpca"]] == "B_cell"  ~ 'Hematopoeitic_cells',
+        meta_LQ_sub[["hpca"]] == "Macrophage"  ~ 'Hematopoeitic_cells',
+        meta_LQ_sub[["hpca"]] == "Monocyte"  ~ 'Hematopoeitic_cells',
+        meta_LQ_sub[["hpca"]] == "NK_cell"  ~ 'Hematopoeitic_cells',
+        meta_LQ_sub[["hpca"]] == "Neutrophils"  ~ 'Hematopoeitic_cells',
+        meta_LQ_sub[["hpca"]] == "Platelets"  ~ 'Hematopoeitic_cells'
+))
+
+
+fwrite(meta_LQ_sub, 'metadata_subsample_LQ.txt', sep='\t') # Export subsampled metadata
+
+
--- 002_HQ_LQ_seurat.r
+++ 002_HQ_LQ_seurat.r
@@ -19,22 +19,6 @@ meta_sub <- as.data.frame(fread('metadata_subsample_HQ.txt', sep='auto')) # Jinf
 rownames(meta_sub) <- meta_sub$Cell
 meta_sub$V1 <- NULL
 
-## assign hpca labels to lineage annotations. JF's annotations "Celltype" are used for lineage annotation of high quality cells 
-meta_sub[["Lineage"]] <- meta_sub$Celltype
-meta_sub <- meta_sub %>% mutate( Lineage = case_when(
-# relabel HQ cells according to JF annotations | Only HQ cells have "Celltype" annotaions
-        meta_sub[["Celltype"]] == "Cancer cells"  ~ 'Epithelial_cells',
-        meta_sub[["Celltype"]] == "Normal epithelial cells"  ~ 'Epithelial_cells',
-        meta_sub[["Celltype"]] == "Adipocytes"  ~ 'Mesenchymal_cells',
-        meta_sub[["Celltype"]] == "Fibroblasts"  ~ 'Mesenchymal_cells',
-        meta_sub[["Celltype"]] == "Endothelial cells"  ~ 'Mesenchymal_cells',
-        meta_sub[["Celltype"]] == "Pericytes"  ~ 'Mesenchymal_cells',
-        meta_sub[["Celltype"]] == "Macrophages"  ~ 'Hematopoeitic_cells',
-        meta_sub[["Celltype"]] == "T cells"  ~ 'Hematopoeitic_cells',
-        meta_sub[["Celltype"]] == "B cells"  ~ 'Hematopoeitic_cells'
-))
-
-
 # export metadata
 fwrite(meta_sub,'metadata_subsample_anno.txt', sep='\t', nThread = 16, row.names = TRUE) # metadata with singleR annotations (subsampled) | contains hpca and blueprint annotations
 
@@ -43,27 +27,10 @@ seu_HQ <- CreateSeuratObject(counts= counts, min.features= 0, min.cells = 0, nam
 qsave(seu_HQ, file="seu_HQ.qs")
 
 
+LQ_count_batches_merged <- as.data.frame(do.call(cbind, count.filter.list))  #join list of tables 
+LQ_count_batches_merged <- LQ_count_batches_merged[,!duplicated(colnames(LQ_count_batches_merged))] # remove duplicate cell id
+LQ_count_batches_merged <- LQ_count_batches_merged[!duplicated(LQ_count_batches_merged$"Gene Symbol"),] # remove duplicate genes
+LQ_count_batches_merged <- LQ_count_batches_merged[LQ_count_batches_merged$'Gene Symbol' != '' ,] # remove null genes
 
-
-# seu_HQ <- qread(file = "seu_HQ.qs", nthreads = 16)
-
-# # # seu_HQ <- SCTransform(seu_HQ, method = "glmGamPoi", verbose = TRUE) # "S", "G2M", removed.
-
-
-# #dim reduction 
-# seu_HQ <- RunPCA(seu_HQ, features = ) # "Features = NULL" to run on variable genes only
-
-
-# #Cell clustering
-# seu_HQ <- FindNeighbors(seu_HQ, dims = 1:20) #Matrix package version issue 
-# seu_HQ <- FindClusters(seu_HQ, resolution = 0.4) # mod from resolution= 0.5 
-
-# # Idents(seu_HQ) <- "Lineage"
-
-# #UMAP
-# seu_HQ <- RunUMAP(seu_HQ, dims = 1:20)
-# DimPlot(seu_HQ, reduction = "umap", group.by = 'Lineage') # infercnv_annotation  FinalAnnotation
-
-
-# ggplot(seu_HQ@meta.data, aes(seu_HQ@meta.data$nFeature_RNA, log10( seu_HQ@meta.data$nCount_RNA))) +
-# geom_point()
+# ordering counts and meta cells alphabetically 
+temp <- LQ_count_batches_merged[,-1]
